efd893eba04ef8d0a373a4dc9924f8c9d751559f
Trzocha <sebastiantrzoska@gmail.com>
2018-11-08 23:45:14 +0100

    Add files via upload

---
+++ precache-manifest.6d4c445f0bac2f561f75507cc1d39bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Szubienica/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7e7ac6ea748f80ee2bc5",
+    "url": "/Szubienica/static/js/runtime~main.7e7ac6ea.js"
+  },
+  {
+    "revision": "9bf78bd72c25c2e587dd",
+    "url": "/Szubienica/static/js/main.9bf78bd7.chunk.js"
+  },
+  {
+    "revision": "7e417a777a4d86431b2a",
+    "url": "/Szubienica/static/js/1.7e417a77.chunk.js"
+  },
+  {
+    "revision": "9bf78bd72c25c2e587dd",
+    "url": "/Szubienica/static/css/main.523b620e.chunk.css"
+  },
+  {
+    "revision": "eb80ca8d4d1a43b3d959596ada35fb16",
+    "url": "/Szubienica/index.html"
+  }
+];
